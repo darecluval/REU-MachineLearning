bd7f76d93f7d2f58f4456e2cd5b792b4b69a621d
Clare DuVal <37857579+darecluval@users.noreply.github.com>
2019-08-23 08:46:55 -0400

    Update mlViolaD.py

--- mlViolaD.py
+++ mlViolaD.py
@@ -1,8 +1,7 @@
 # Machine Learning - Viola D - D Major
 # Clare DuVal - MSU/Clemson
-# Updated: August 23, 2019
+# Updated: August 23. 2019
 
-#IMPORTS
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.training import checkpoint_state_pb2
@@ -18,8 +17,8 @@ import helper as helper
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-#NETWORK PARAMETERS
-epochs = 10
+# Network Parameters
+epochs = 2000
 learning_rate = 0.01
 batch_size = 156
 batch_size2 = batch_size / 2
@@ -28,32 +27,36 @@ print("Number of Epochs: " + str(epochs))
 print("Learning Rate: " + str(learning_rate))
 print("Batch Size: " + str(batch_size))
 
-#Variables
+if input("Would you like to use a drop-based learning rate? (y/n)") = "y":
+    lr_drop = input("What percentage would you like to be the drop in percentage per epoch? (0.01-.99)")
+else:
+    lr_drop = 1.0
+
 train_num = 56
 test_num = 70 - train_num
 num_nets = 9
 num_classes = 13
-dim = 200 #height and width
+dim = 200 # Height and width
 
-#-------Input Train and Test Data-------#
+### Input Train and Test Data
 
-#Pre-Allocating Images
+# Pre-Allocating Images
 train_images = np.zeros((num_sets * train_num * num_classes, dim*dim))
 train_labels = np.zeros((num_sets * train_num * num_classes, num_classes))
 test_images = np.zeros((num_sets * test_num * num_classes, dim*dim))
 test_labels = np.zeros((num_sets * test_num * num_classes, num_classes))
 
-#Randomly shuffles notes to be collected
+# Randomly shuffles notes to be collected
 notes = list(range(1,71))
 np.random.shuffle(notes)
-#print(notes)
 
-#Change working directory
-PATH = "/Users/clareduval/Documents/Python/Spectrograms"
-os.chdir(PATH)
+# Change working directory
+dir_path = "/Users/clareduval/Documents/Python/Spectrograms"
+os.chdir(dir_path)
 
-#Files to be read are in format "set_note_j.jpeg"
-#Training Data
+# Precondition: Files to be read are in format "set_note_j.jpeg"
+
+# Training Data
 count = 0
 for j in range(train_num):
     for set in range(num_sets):
@@ -77,16 +80,19 @@ for k in range(test_num):
             test_labels[count, note] = 1
             count += 1
 
-#Shuffles images and labels simultaneously
+#Shuffles images and labels simultaneously, then convert to tensors
 train_random = list(zip(train_images, train_labels))
 random.shuffle(train_random)
 train_images, train_labels = zip(*train_random)
+train = np.asarray(train_images, np.float32)
+train_x = train.reshape(-1, dim, dim, 1)
 
 test_random = list(zip(test_images, test_labels))
 random.shuffle(test_random)
 test_images, test_labels = zip(*test_random)
+test = np.asarray(test_images, np.float32)
+test_x = test.reshape(-1, dim, dim, 1)
 
-#Label Dictionary
 labels = {
     1: 'Detache Down',
     2: 'Detache Up',
@@ -103,50 +109,38 @@ labels = {
     13: 'Ricochet'
 }
 
-#Convert Images to Tensors
-train = np.asarray(train_images, np.float32)
-test = np.asarray(test_images, np.float32)
-train_x = train.reshape(-1, dim, dim, 1)
-test_x = test.reshape(-1, dim, dim, 1)
-
 print("Images imported successfully!")
 
 #Placeholder
 x = tf.placeholder("float", [None, dim, dim, 1])
 y = tf.placeholder("float", [None, num_classes])
 
-#Weights
 weights = {
     'wc1': tf.get_variable('W0', shape = (3, 3,  1,  batch_size), initializer = tf.contrib.layers.xavier_initializer()),
     'wc2': tf.get_variable('W1', shape = (3, 3, batch_size,  batch_size2), initializer = tf.contrib.layers.xavier_initializer()),
     'wc3': tf.get_variable('W2', shape = (3, 3, batch_size2, batch_size4), initializer = tf.contrib.layers.xavier_initializer()),
     'wd1': tf.get_variable('W3', shape = (5*5*batch_size4, batch_size4), initializer = tf.contrib.layers.xavier_initializer()),
-    'out': tf.get_variable('W6', shape = (batch_size4, num_classes), initializer = tf.contrib.layers.xavier_initializer())
-}
-
-#Biases
+    'out': tf.get_variable('W6', shape = (batch_size4, num_classes), initializer = tf.contrib.layers.xavier_initializer())}
 biases = {
     'bc1': tf.get_variable('B0', shape = (batch_size), initializer = tf.contrib.layers.xavier_initializer()),
     'bc2': tf.get_variable('B1', shape = (batch_size2), initializer = tf.contrib.layers.xavier_initializer()),
     'bc3': tf.get_variable('B2', shape = (batch_size4), initializer = tf.contrib.layers.xavier_initializer()),
     'bd1': tf.get_variable('B3', shape = (batch_size4), initializer = tf.contrib.layers.xavier_initializer()),
-    'out': tf.get_variable('B4', shape = (num_classes), initializer = tf.contrib.layers.xavier_initializer())
-}
+    'out': tf.get_variable('B4', shape = (num_classes), initializer = tf.contrib.layers.xavier_initializer())}
 
-#Loss and Optimizer Nodes
+# Loss and Optimizer Nodes
 pred = helper.conv_net(x, weights, biases)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = pred, labels = y))
 optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
 
-#Evaluate Model Node
+# Evaluate Model Node
 correct_prediction = tf.equal(tf.argmax(pred,1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-#Initialize the variables
+# Initialize the variables
 init = tf.global_variables_initializer()
 
-#---TRAIN & TEST THE MODEL---#
-i = 1
+### Train & Test the Model
 with tf.Session() as sess:
 
     sess.run(init)
@@ -182,6 +176,6 @@ with tf.Session() as sess:
         test_accuracy.append(test_acc)
 
         print("Testing Accuracy: ", "{:.5f}".format(test_acc))
-        learning_rate = learning_rate * 0.85
+        learning_rate = learning_rate * lr_drop
 
     summary_writer.close()
